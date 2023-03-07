e50a396a5ea147a39f7b9d86bc25010af01ad218
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-03-07 11:13:20 +0100

    Play a video

--- wma_2/object_tracker.py
+++ wma_2/object_tracker.py
@@ -2,13 +2,27 @@
 This is a script that will track red object on the screen.
 '''
 import cv2
+import numpy as np
 
-def display_image():
-    image = cv2.imread('materials\example.jpg')
-    window_name = 'image'
-    cv2.imshow(window_name, image)
-    cv2.waitKey(0)
+def display_video():
+    cap = cv2.VideoCapture("materials/rgb_balls.mp4")
+    
+    if (cap.isOpened() == False):
+        print("ERROR")
+    
+    while (cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            cv2.imshow('Frame', frame)
+
+            if cv2.waitKey(25) == ord('q'):
+                break
+
+        else:
+            break
+
+    cap.release()
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    display_image()
+    display_video()
