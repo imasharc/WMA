242608c0de5915920f7cba0057c4441a821408de
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-03-08 11:43:41 +0100

    copy-paste from 'opencv_demo' solution with basic modification - somewhat works but I have no idea what's going on yet

--- wma_2/object_tracker.py
+++ wma_2/object_tracker.py
@@ -25,10 +25,27 @@ def display_video():
             red_mask = cv2.inRange(hsv_red, low_red, high_red)
             red = cv2.bitwise_and(frame, frame, mask=red_mask)
 
+            canny_output = cv2.Canny(red, 100, 200)
+            red_contours, _ = cv2.findContours(canny_output, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+            contours_poly = [None]*len(red_contours)
+            boundRect = [None]*len(red_contours)
+            for i, c in enumerate(red_contours):
+                contours_poly[i] = cv2.approxPolyDP(c, 3, True)
+                boundRect[i] = cv2.boundingRect(contours_poly[i])
+
+            for i in range(len(red_contours)):
+                color = (rng.randint(0, 256), rng.randint(
+                    0, 256), rng.randint(0, 256))
+                # cv2.drawContours(frame, contours_poly, i, color)
+                if abs(boundRect[i][0]-boundRect[i][1]) < 200 and abs(boundRect[i][2]-boundRect[i][3]) < 200:
+                    cv2.rectangle(frame, (int(boundRect[i][0]), int(boundRect[i][1])),
+                                (int(boundRect[i][0]+boundRect[i][2]), int(boundRect[i][1]+boundRect[i][3])), color, 2)
+
             cv2.imshow('Frame', frame)
             cv2.imshow('red', red)
             # cv2.imshow('MASK', black_stuff)
-            # cv2.imshow('EDGES', canny_output)
+            cv2.imshow('EDGES', canny_output)
 
             if cv2.waitKey(25) == ord('q'):
                 break
