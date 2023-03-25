7e5619ca1ad3d360bb0de866b72d57f23c0f4e7c
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-03-25 22:33:08 +0100

    Add simple GUI window for choosing a video/image

--- wma_4/holes_detector.py
+++ wma_4/holes_detector.py
@@ -1,3 +1,73 @@
+#===================================================
+#                       IMPORTS
+#===================================================
+
 import cv2
 import numpy as np
-import tkinter as th # Python GUI library
+import tkinter as tk # Python GUI library
+import tkinter.filedialog # This must be imported explicitly to use filedialog
+from PIL import Image
+from PIL import ImageTk
+
+#===================================================
+#                         GUI
+#===================================================
+
+class GUI_MainWindow:
+    def __init__(self):
+        self.root = tk.Tk()
+
+        self.video = None
+        self.panel_a = None
+
+        self.btn = tk.Button(self.root, text = 'Select a video', command = self.load_video)
+        self.btn.pack(side = 'bottom', fill = 'both', expand = 'yes', padx = '10', pady = '10')
+
+    def load_video(self):
+        path = tk.filedialog.askopenfile()
+        self.video = cv2.VideoCapture(path.name)
+        print(f'Path to the file is {path.name}')
+        self.root.after(33, self.update)
+        # self.update()
+    
+    def read_frame_from_video(self):
+        if self.video is None:
+            return None
+        
+        ret, frame = self.video.read()
+        if not ret:
+            return None
+        return frame
+        
+    def update_video_display(self, frame):
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image = Image.fromarray(image)
+        image = image.resize((800, 600), Image.ANTIALIAS)
+        image = ImageTk.PhotoImage(image)
+
+        if self.panel_a is None:
+            self.panel_a = tk.Label(image = image)
+            self.panel_a.image = image
+            self.panel_a.pack(side = 'left', padx = 10, pady = 10)
+        else:
+            self.panel_a.configure(image = image)
+            self.panel_a.image = image
+
+    def update(self):
+        frame = self.read_frame_from_video()
+        if frame is not None:
+            self.update_video_display(frame)
+
+    def run(self):
+        self.root.mainloop()
+
+#===================================================
+#                   MAIN FUNCTION
+#===================================================
+
+def main():
+    main_window = GUI_MainWindow()
+    main_window.run()
+
+if __name__ == '__main__':
+    main()
