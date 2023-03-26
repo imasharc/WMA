fba873057c272d78b78e81b5a5e738086e951dbd
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-03-26 13:44:23 +0200

    Add circle detection using Hough Transform - poor outcome, TO BE DONE - implement circle detection from scratch

--- wma_4/holes_detector.py
+++ wma_4/holes_detector.py
@@ -10,14 +10,36 @@ from PIL import Image
 from PIL import ImageTk
 
 #===================================================
+#                   CIRCLE DETECTION
+#===================================================
+
+class CircleDetector:
+
+    def __init__(self, gradient_variant, dp, min_distance, param_1 = 60, param_2 = 40, min_radius = 0, max_radius = 0):
+        self.gradient_variant = gradient_variant
+        self.dp = dp
+        self.min_distance = min_distance
+        self.param_1 = param_1
+        self.param_2 = param_2
+        self.min_radius = min_radius
+        self. max_radius = max_radius
+
+    def detect(self, image):
+        circles = cv2.HoughCircles(image, self.gradient_variant, self.dp, self.min_distance,
+                                param1 = self.param_1, param2 = self.param_2,
+                                minRadius = self.min_radius, maxRadius = self.max_radius)
+        return np.uint16(np.around(circles))
+
+#===================================================
 #                         GUI
 #===================================================
 
 class GUI_MainWindow:
-    def __init__(self, refresh_rate = 33):
+    def __init__(self, circle_detector, refresh_rate = 33):
         self.root = tk.Tk()
 
         self.refresh_rate = refresh_rate
+        self.circle_detector = circle_detector
 
         self.video = None
         self.panel_a = None
@@ -42,6 +64,14 @@ class GUI_MainWindow:
         return frame
         
     def update_video_display(self, frame):
+
+        frame = cv2.resize(frame, (360, 360))
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        circles = self.circle_detector.detect(gray)
+        for i in circles[0,:]:
+            cv2.circle(frame, (i[0], i[1]), i[2], (0, 255, 0), 2)
+            cv2.circle(frame, (i[0], i[1]), 2, (0, 0, 255), 3)
+
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         image = Image.fromarray(image)
         image = image.resize((800, 600), Image.Resampling.LANCZOS)
@@ -51,6 +81,7 @@ class GUI_MainWindow:
             self.panel_a = tk.Label(image = image)
             self.panel_a.image = image
             self.panel_a.pack(side = 'left', padx = 10, pady = 10)
+            print(circles)
         else:
             self.panel_a.configure(image = image)
             self.panel_a.image = image
@@ -69,7 +100,8 @@ class GUI_MainWindow:
 #===================================================
 
 def main():
-    main_window = GUI_MainWindow()
+    circle_detector = CircleDetector(cv2.HOUGH_GRADIENT, 1, 20, param_1 = 20, param_2 = 80)
+    main_window = GUI_MainWindow(circle_detector)
     main_window.run()
 
 if __name__ == '__main__':
