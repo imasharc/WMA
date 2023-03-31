360816e64b75c8bc8dbd415183d1876b7a3d511f
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-03-31 22:38:30 +0200

    Add Harris corner and edge detection - inspired by the video at https://odysee.com/@regule:9/harris-numpy:8?r=D6qowKCqxdVu2ivcZb9mNREB3rppHhmx (implementation for an image); TO BE DONE - reduce slugginess of playback, show both corners and edges inside a single frame

--- wma_4/corner_detection.py
+++ wma_4/corner_detection.py
@@ -26,7 +26,7 @@ def show_img(img, bw = False):
     ax = fig.gca()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
-    ax.imshow(img, cmap = 'Greys_r' if bw else None)
+    ax.imshow(img, cmap = 'Greys_r')
     plt.show()
 
 #===================================================
@@ -34,8 +34,63 @@ def show_img(img, bw = False):
 #===================================================
 
 def main():
-    img = cv2.imread('materials/box_2.png')
-    show_img(img)
+    video = cv2.VideoCapture("materials/spongebob.mp4")
+
+    while (video.isOpened()):
+        ret, frame = video.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        kernel_x = np.array([
+        [-1, 0, 1],
+        [-2, 0, 2],
+        [-1, 0, 1]
+        ])
+        kernel_y = np.array([
+            [1, 2, 1],
+            [0, 0, 0],
+            [-1, -2, -1]
+        ])
+        derrivative_x = sig.convolve2d(gray, kernel_x, mode='same')
+        derrivative_y = sig.convolve2d(gray, kernel_y, mode='same')
+        Ixx = ndi.gaussian_filter(derrivative_x ** 2, sigma = 1)
+        Ixy = ndi.gaussian_filter(derrivative_y * derrivative_x, sigma = 1)
+        Iyy = ndi.gaussian_filter(derrivative_y ** 2, sigma = 1)
+        # parameter for Harris corner detection
+        k = 0.05
+        detA = Ixx * Iyy - Ixy ** 2
+        traceA = Ixx + Iyy
+        harris_response = detA - k * traceA ** 2
+        # print(f'Min = {harris_response.min()} Max = {harris_response.max()}')
+        harris_response_range = harris_response.max() - harris_response.min()
+        scaled_response = (harris_response / harris_response_range) * 255
+
+        # show_img(scaled_response)
+        corners = np.copy(frame)
+        edges = np.copy(frame)
+        h_min = harris_response.min()
+        h_max = harris_response.max()
+        THRESHOLD_CORNER = 0.01
+        THRESHOLD_EDGE = 0.01
+
+        if ret == True:
+            for y, row in enumerate(harris_response):
+                for x, pixel in enumerate(row):
+                    if pixel > h_max * THRESHOLD_CORNER:
+                        corners[y, x] = [0, 0, 255]
+                    elif pixel <= h_min * THRESHOLD_EDGE:
+                        edges[y, x] = [255, 255, 255]
+        
+        res = cv2.addWeighted(corners, 0.5, edges, 0.5, 0)
+        cv2.imshow('spongebob', res)
+        # cv2.imshow('spongebob', edges)
+        # cv2.imshow('EDGES', canny_output)
+
+        if cv2.waitKey(25) == ord('q'):
+            break
+
+    video.release()
+    cv2.destroyAllWindows()
+    # show_img(corners)
+    # show_img(edges)
 
 if __name__ == '__main__':
     main()
