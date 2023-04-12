d0aeda86008afeedbeb1982cc54c9215cf2d1029
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-04-12 19:51:52 +0200

    Reformat code, add sections

--- wma_5_6/sift_script.py
+++ wma_5_6/sift_script.py
@@ -1,5 +1,27 @@
+#===================================================
+#                     DESCRIPTION
+#===================================================
+
+# The program aims to implement SIFT algorithm from scratch,
+# which is a feature detection algorithm in Computer Vision
+# (Scale Invariant Feature Transform)
+# above info source: https://www.analyticsvidhya.com/blog/2019/10/detailed-guide-powerful-sift-technique-image-matching-python/
+
+#===================================================
+#                       IMPORTS
+#===================================================
+
 import cv2 as cv
+
+#===================================================
+#                   CONFIGURATION
+#===================================================
+
 SOURCE_IMAGE_PATH = 'materials\lab_5_6\source.png'
+
+#===================================================
+#                   MAIN FUNCTION
+#===================================================
 
 def main():
     source_image = cv.imread(SOURCE_IMAGE_PATH)
