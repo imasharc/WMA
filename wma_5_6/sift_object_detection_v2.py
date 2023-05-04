a23d27a37cf41851ece68e8047c8eb2fa29c7b2d
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-05-04 13:05:24 +0200

    Detect and compute keypoints for the source_image, display source_image_2 with marked keypoints

--- wma_5_6/sift_object_detection_v2.py
+++ wma_5_6/sift_object_detection_v2.py
@@ -14,11 +14,57 @@
 # in detecting the object on the target video
 
 #===================================================
+#                       IMPORTS
+#===================================================
+
+import cv2
+import numpy as np
+from sklearn.cluster import AffinityPropagation
+
+#===================================================
+#                     PARAMETERS
+#===================================================
+
+SOURCE_IMAGE_PATH = 'materials\lab_5_6\saw1.jpg'
+SOURCE_IMAGE_PATH_2 = 'materials\lab_5_6\saw2.jpg'
+SOURCE_IMAGE_PATH_3 = 'materials\lab_5_6\saw3.jpg'
+SOURCE_IMAGE_PATH_4 = 'materials\lab_5_6\saw4.jpg'
+TARGET_IMAGE_PATH = 'materials\lab_5_6\saw4.jpg'
+TARGET_VIDEO_PATH = 'materials\lab_5_6\sawmovie.mp4'
+FLANN_INDEX_KDTREE = 1
+FLANN_TREES = 5
+FLANN_CHECKS = 50
+FLANN_K = 2
+KEYPOINT_VALIDITY_THRESHOLD = 0.68
+AFFIINITY_DAMPING = 0.94
+
+#===================================================
+#                   TRAINING STAGE
+#===================================================
+
+def sift():
+    sift = cv2.SIFT_create()
+
+    source_image = cv2.imread(SOURCE_IMAGE_PATH)
+    source_image = cv2.resize(source_image, dsize=(600, 600))
+    gray_source = cv2.cvtColor(source_image, cv2.COLOR_BGR2GRAY)
+    
+    source_image_2 = cv2.imread(SOURCE_IMAGE_PATH_2)
+    source_image_2 = cv2.resize(source_image, dsize=(600, 600))
+    gray_source_2 = cv2.cvtColor(source_image_2, cv2.COLOR_BGR2GRAY)
+    
+    source_keypoints, source_descriptors = sift.detectAndCompute(gray_source_2, None)
+    marked_target = cv2.drawKeypoints(source_image_2, source_keypoints, None)
+    cv2.imshow('Target', marked_target)
+
+    cv2.waitKey()
+
+#===================================================
 #                   MAIN FUNCTION
 #===================================================
 
 def main():
-    print('Hello')
+    sift()
 
 if __name__ == "__main__":
     main()
