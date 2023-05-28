1c5d5c167bdb5e6b5674d667e115f5f1dba8b606
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-05-28 12:47:53 +0200

    Display pokemon images
    Display and entitle pokemon images (limits to 8)

--- wma_7_8/pokedex.py
+++ wma_7_8/pokedex.py
@@ -13,7 +13,35 @@ It was created with pokemon dataset in mind but can be used for other datasets.
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.image as mimg
 import argparse
+import os               # provides operating system specific functions
+
+#===================================================
+#                   ARGUMENT PARSER
+#===================================================
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-d', '--description_file', default='pokemon/pokemon.csv', help='A CSV file with pokemon informations')
+    parser.add_argument('-i', '--image_folder', default='pokemon/images', help='Folder with pokemon images')
+    return parser.parse_args()
+
+#===================================================
+#               DISPLAY POKEMON IMAGES
+#===================================================
+
+def show_example_images(image_folder):
+    images = os.listdir(image_folder)
+    fig, axes = plt.subplots(2, 4)
+    axes = axes.flatten()
+    for idx, img_file in enumerate(images):
+        if idx >= len(axes):
+            break
+        img = mimg.imread(os.path.join(image_folder, img_file))
+        axes[idx].imshow(img)
+        axes[idx].set_title(img_file.split('.')[0])
+    plt.show()
 
 #===================================================
 #                   MAIN FUNCTION
@@ -23,16 +51,13 @@ def show_dataset_info(pokedex):
     print(pokedex.info())
     print(pokedex.head())
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-d', '--description_file', default='pokemon/pokemon.csv', help='A CSV file with pokemon informations')
-    parser.add_argument('-i', '--image_path', default='pokemon/images/images', help='Folder with pokemon images')
-    return parser.parse_args()
-
 def main():
     args = parse_arguments()
     pokedex = pd.read_csv(args.description_file)
+    pokedex.drop('Type2', axis=1, inplace=True)
+    pokedex.sort_values(by=['Name'], ascending=True, inplace=True)
     show_dataset_info(pokedex)
+    show_example_images(args.image_folder)
 
 if __name__ == '__main__':
     main()
